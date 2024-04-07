346b622667b7f3d459e02c4bcad4ce21fb0a9b81
AlexaAlexandru <liv06ale@gmail.com>
2024-04-07 12:10:54 +0300

    feat: connection to mongoDb

--- DashBoard-ND_API/Program.cs
+++ DashBoard-ND_API/Program.cs
@@ -1,15 +1,24 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+string? connectionString = configuration.GetConnectionString("MongoDb");
+
+var settings = MongoClientSettings.FromConnectionString(connectionString);
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+var mongoClient = new MongoClient(settings);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IMongoClient>(mongoClient);
+builder.Logging.AddConsole();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,10 +26,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-app.Run();
+try
+{
+    var result = mongoClient.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    app.Logger.LogInformation("Pinged the MongoDB deployment. Successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    app.Logger.LogError(ex, "Failed to connect to MongoDB.");
+}
 
+app.Run();
